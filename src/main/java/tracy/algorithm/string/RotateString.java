c16e1e413e6be18e69588be4064e35c3b68554ca
madtracy <madtracy@gmail.com>
2015-04-06 20:39:24 +0800

    调整名称

--- src/main/java/tracy/algorithm/string/RotateString.java
+++ src/main/java/tracy/algorithm/string/RotateString.java
@@ -1,13 +1,14 @@
 package tracy.algorithm.string;
 
 /**
- * Created with IntelliJ IDEA.
  * User: tracy
  * Date: 14-8-17
  * Time: 下午3:16
- * To change this template use File | Settings | File Templates.
+ * 给定一个字符串，要求把字符串前面的若干个字符移动到字符串的尾部，
+ * 如把字符串“abcdef”前面的2个字符'a'和'b'移动到字符串的尾部，使得原字符串变成字符串“cdefab”。
+ * 请写一个函数完成此功能，要求对长度为n的字符串操作的时间复杂度为 O(n)，空间复杂度为 O(1)。
  */
-public class LeftRotate {
+public class RotateString {
 
     public static String rotate(String str,int pos){
         if(str==null || str.isEmpty() || pos<0 || pos>=str.length()){
